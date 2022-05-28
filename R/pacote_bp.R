27bcb1a071e6ec5b3bf88e8f4a4955844328e2a7
Juciane <jhucyy.pereira@gmail.com>
2022-05-28 19:20:08 -0300

    atualizações pacote bp, contém a primeira função

---
+++ R/pacote_bp.R
@@ -0,0 +1,47 @@
+# ideia preliminar, buscar os vídeos mais assistidos da BP
+library(usethis)
+library(tidyverse)
+library(lubridate)
+library(devtools)
+# base de dados -----------------------------------------------------------
+library(readxl)
+brasil_paralelo <- readxl::read_excel("./brasil_paralelo.xls")
+
+# Carregar as funções e checar o script -----------------------------------
+
+devtools::load_all()
+devtools::check()
+
+# base de lista de vídeos extraído do canal do youtube da Brasil Paralelo
+# em 28/05/2022, base extraída com o programa Youtube Data Tools
+
+
+# Função filtrar vídeos da base de dados Brasil Paralelo ------------------
+# A função filtra os vídeos com base na categoria, mes e no ano.
+
+
+# função para filtrar as categorias dos vídeos ----------------
+
+filtrar_videos_por_categorias <- function(filtro_categorias,
+                                          filtro_mes, filtro_ano){
+   brasil_paralelo %>%  #base de dados
+    dplyr::mutate(ano = lubridate::year(data_de_publicação),
+                  mes = lubridate::month(data_de_publicação)) %>%
+       group_by(categorias, mes, ano) %>%
+       ungroup() %>%
+  dplyr::filter(categorias == filtro_categorias) %>%
+    dplyr::filter(ano == filtro_ano & mes == filtro_mes)
+
+}
+
+
+# ex:
+filtrar_videos_por_categorias(filtro_categorias = "Educação", filtro_mes = 01,
+                              filtro_ano = 2021)
+
+
+
+
+# Função02: Filtrar os vídeos com base no Número de likes e coment --------
+
+
---
+++ README.md
@@ -0,0 +1,26 @@
+
+# meupacote
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of meupacote is to ...
+
+## Installation
+
+You can install the development version of meupacote from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("jhucyy/meupacote")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(meupacote)
+## basic example code
+```
+
