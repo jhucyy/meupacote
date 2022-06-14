18014d5ff8f44d76d77115cda5c7ceeb02a2ef76
jhucyy <jhucyy.pereira@gmail.com>
2022-06-14 00:01:54 -0300

    documentação

--- R/function-buscar-videos-por-categorias.R
+++ R/function-buscar-videos-por-categorias.R
@@ -1,4 +1,19 @@
 
+#' Filtrar vídeos pelas categorias
+#'
+#' Essa função tem como objetivo filtrar a base de vídeos extraída do canal
+#' Brasil Paralelo a partir do critério de categoria, estabelecida conforme
+#' a classificação do YouTube, junto aos critérios de mês e ano do vídeo
+#' em questão.
+#'
+#' @param filtro_categorias filtra as categorias, categorias disponíveis "Educação", "Política", "Entretenimento" e "Diversos".
+#' @param filtro_mes filtra os vídeos com base no mês.
+#' @param filtro_ano filtra o vídeo com base no ano.
+#'
+#' @return um data frame
+#' @export
+#'
+#' @examples filtrar_videos_por_categorias(filtro_categorias = "Educação", filtro_mes = 02, filtro_ano = 2022)
 filtrar_videos_por_categorias <- function(filtro_categorias,
                                           filtro_mes, filtro_ano){
   brasil_paralelo %>%
@@ -10,5 +25,7 @@ filtrar_videos_por_categorias <- function(filtro_categorias,
     dplyr::filter(ano == filtro_ano & mes == filtro_mes)
 
 }
+
+
 
 
--- R/function-filtrar-videos-por-rel.R
+++ R/function-filtrar-videos-por-rel.R
@@ -1,4 +1,21 @@
 
+#' Filtrar vídeos pelas métricas de relevância
+#'
+#' Essa função tem como objetivo simplificar a triagem dos vídeos da base
+#'  do canal Brasil Paralelo, com base em dois critérios que enumerei
+#' para representar o grau de relevância dos vídeos, o número de likes
+#' e o número de comentários dos vídeos. Os critérios devem ser designados
+#' conforme a numeração escolhida, e colocados nos argumentos filtro_likes = x
+#' para o número de likes desejados e filtro_comen = x para o número de
+#' comentários estabelecido.
+#'
+#' @param filtro_likes  quantidade de likes
+#' @param filtro_comen  quantidade de comentários
+#'
+#' @return um data frame
+#' @export
+#'
+#' @examples filtrar_rel(filtro_likes = 1000, filtro_comen = 200)
 filtrar_rel <- function(filtro_likes, filtro_comen){
   brasil_paralelo %>%
   dplyr::filter(likes >= filtro_likes, comentários >= filtro_comen)
